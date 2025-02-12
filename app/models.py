fbd4f775f43e572ddba21e2ddd8341bf1bbb5b41
Sujai Kumar Gupta <sujayscience1234@gmail.com>
2025-02-12 12:38:56 +0530

    chore: Add User models and validation logic

---
+++ app/models.py
@@ -0,0 +1,44 @@
+from typing import Optional
+
+from bson import ObjectId
+from pydantic import BaseModel, EmailStr, Field
+
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+
+class UserBase(BaseModel):
+    name: str
+    email: EmailStr
+    age: Optional[int] = None
+
+
+class UserCreate(UserBase):
+    pass
+
+
+class UserUpdate(BaseModel):
+    name: Optional[str]
+    email: Optional[EmailStr]
+    age: Optional[int]
+
+
+class UserInDB(UserBase):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+
+    class Config:
+        json_encoders = {ObjectId: str}
+        allow_population_by_field_name = True
